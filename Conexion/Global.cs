95f9dc2b4220d978697340e4bca5ec4424a49a73
gutherduarte <cgdr010601@gmail.com>
2022-11-23 09:00:53 -0600

    add docker compose

--- Conexion/Global.cs
+++ Conexion/Global.cs
@@ -9,9 +9,7 @@ namespace backend_especial.Conexion
         static string DATABASE = Environment.GetEnvironmentVariable("DATABASE");
         static string USERNAME = Environment.GetEnvironmentVariable("USERNAME");
         static string PASSWORD = Environment.GetEnvironmentVariable("PASSWORD");
-        static string INTEGRATED_SECURITY = Environment.GetEnvironmentVariable("INTEGRATED_SECURITY");
-        static string TRUST_SERVER_CERTIFICATE = Environment.GetEnvironmentVariable("TRUST_SERVER_CERTIFICATE");
 
-        public static string ConnectionString = $"Server={SERVER},{PORT}; Initial Catalog={DATABASE};Persist Security Info=False; User ID={USERNAME};Password={PASSWORD};MultipleActiveResultSets=False; Encrypt=True;Integrated Security={INTEGRATED_SECURITY};";
+        public static string ConnectionString = $"Server={SERVER},{PORT}; Initial Catalog={DATABASE};Persist Security Info=False; User ID={USERNAME};Password={PASSWORD};MultipleActiveResultSets=False; Encrypt=True;";
     } 
 }
