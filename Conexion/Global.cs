43fb54296901233673c45a44f2c1ab6a5dd1cacb
gutherduarte <cgdr010601@gmail.com>
2022-11-23 08:06:09 -0600

    fix connection string

--- Conexion/Global.cs
+++ Conexion/Global.cs
@@ -1,15 +1,17 @@
-﻿namespace backend_especial.Conexion
+﻿using System;
+
+namespace backend_especial.Conexion
 {
     public class Global
     {
-        string SERVER = Environment.GetEnvironmentVariable("SERVER");
-        string PORT = Environment.GetEnvironmentVariable("PORT");
-        string DATABASE = Environment.GetEnvironmentVariable("DATABASE");
-        string USERNAME = Environment.GetEnvironmentVariable("USERNAME");
-        string PASSWORD = Environment.GetEnvironmentVariable("PASSWORD");
-        string INTEGRATED_SECURITY = Environment.GetEnvironmentVariable("INTEGRATED_SECURITY");
-        string TRUST_SERVER_CERTIFICATE = Environment.GetEnvironmentVariable("TRUST_SERVER_CERTIFICATE");
+        static string SERVER = Environment.GetEnvironmentVariable("SERVER");
+        static string PORT = Environment.GetEnvironmentVariable("PORT");
+        static string DATABASE = Environment.GetEnvironmentVariable("DATABASE");
+        static string USERNAME = Environment.GetEnvironmentVariable("USERNAME");
+        static string PASSWORD = Environment.GetEnvironmentVariable("PASSWORD");
+        static string INTEGRATED_SECURITY = Environment.GetEnvironmentVariable("INTEGRATED_SECURITY");
+        static string TRUST_SERVER_CERTIFICATE = Environment.GetEnvironmentVariable("TRUST_SERVER_CERTIFICATE");
 
-        public static string ConnectionString = $"Server={SERVER},{PORT}; Initial Catalog={DATABASE};Persist Security Info=False; User ID={USERNAME};Password={PASSWORD};MultipleActiveResultSets=False; Encrypt=True;Integrated Security={INTEGRATED_SECURITY};"
+        public static string ConnectionString = $"Server={SERVER},{PORT}; Initial Catalog={DATABASE};Persist Security Info=False; User ID={USERNAME};Password={PASSWORD};MultipleActiveResultSets=False; Encrypt=True;Integrated Security={INTEGRATED_SECURITY};";
     } 
 }
